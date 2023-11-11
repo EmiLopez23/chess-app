ed262f48111b4fa0a27172896ef9636c0da43f75
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-11-11 12:39:20 -0300

    add promotion to chess game

--- app/src/main/java/games/chess/mover/Mover.java
+++ app/src/main/java/games/chess/mover/Mover.java
@@ -2,6 +2,9 @@ package games.chess.mover;
 
 import common.*;
 import common.Game;
+import common.enums.Color;
+import common.enums.PieceType;
+import games.chess.factory.PieceFactory;
 import games.chess.validators.CheckValidator;
 
 import java.util.ArrayList;
@@ -11,24 +14,41 @@ import java.util.Map;
 
 public class Mover implements SimpleMover {
 
-    CheckValidator checkValidator = new CheckValidator();
+    private final CheckValidator checkValidator = new CheckValidator();
+    private final PieceFactory pieceFactory = new PieceFactory();
     @Override
     public MoveResponse move(Game game, Coordinate from, Coordinate to) {
         Board currentBoard = game.getBoard();
         Piece currentPiece = currentBoard.getBoard().get(from);
         if(currentPiece.getValidator().isValid(game.getBoards(), from, to)) {
             Map<Coordinate, Piece> newPieces = new HashMap<>(currentBoard.getBoard());
-            newPieces.put(to, currentPiece);
+            tryPromotion(currentBoard,newPieces,currentPiece, to);
             newPieces.remove(from);
             Board newBoardObject = new Board(currentBoard.getRowSize(), currentBoard.getColumnSize(), newPieces);
             Game newGame = getNewGame(game, newBoardObject);
             if(!checkValidator.isValid(newGame.getBoards(), from, to)){
-            return new MoveResponse(game, "Check");
+                return new MoveResponse(game, "Check");
             }
             return new MoveResponse(newGame, null);
         }
         return new MoveResponse(game, "Invalid movement");
 
+    }
+
+    private void tryPromotion(Board board, Map<Coordinate, Piece> pieces, Piece currentPiece, Coordinate to) {
+        String pieceId = currentPiece.getId();
+        Color color = currentPiece.getColor();
+        PieceType pieceType = currentPiece.getPieceType();
+        if( pieceType == PieceType.PAWN && (to.row() == board.getRowSize() || to.row() == 1)) {
+            if (color == Color.WHITE) {
+                pieces.put(to, pieceFactory.createWhiteQueen(pieceId));
+            }
+            else {
+                pieces.put(to, pieceFactory.createBlackQueen(pieceId));
+            }
+            return;
+        }
+        pieces.put(to, currentPiece);
     }
 
     private Game getNewGame(Game game, Board newBoard){
